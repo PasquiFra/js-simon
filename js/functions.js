0a8cb956daa300d3bae866045bf7dbd67f7795c5
Your Name <you@example.com>
2024-05-16 11:22:57 +0200

    general fixes, comments, improvements

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-
+// genero un numero casuale da 1 a 99 
 const getUniqueRandomNumber = (min, max, listOfGenNumbers) => {
     let randomNumber;
     //continua a pescare numeri fintanto che quello che hai pescato è già nella blacklist
@@ -8,36 +8,42 @@ const getUniqueRandomNumber = (min, max, listOfGenNumbers) => {
     return randomNumber
 }
 
+// genero le celle con i numeri da ricordare
 const generateCell = (content) => {
     const cell = document.createElement("div");
     cell.className = "magic-numbers num-cell m-3 flex-center";
     cell.innerText = content;
     return cell;
 }
+
+// genero le celle di input
 const generateMyCell = () => {
-    const code = 
-    `
+    const code =
+        `
     <input type="number" class="my-inputs form-control num-cell m-3 flex-center" min="1" max="99" required>
     `;
     cell = code;
     return cell;
 }
 
+//setto la funzione di timer
 const timerFunction = (count) => {
-   
+
     let remainingTime = count;
     const addBlur = document.querySelectorAll(".magic-numbers");
 
-    if (count === 0){
+    //Se il tempo è finito....
+    if (count === 0) {
         clearInterval(count);
-        for (let i = 0 ; i < totCells ; i++){
+        for (let i = 0; i < totCells; i++) {
             addBlur[i].classList.add("blur");
         }
         message.innerHTML = "Inserisci i numeri che hai memorizzato... <br> Vediamo se hai una buona memoria!"
         myNums.classList.remove("d-none");
         submitButton.classList.remove("d-none");
 
-    } else {
+    } else //...altrimenti decremento il timer
+    {
         --remainingTime;
         console.log(remainingTime)
     }
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-console.log ("JS OK");
+console.log("JS OK");
 
 // Dichiaro le variabili dal DOM
 
@@ -26,73 +26,84 @@ timer.textContent = timerCount
 //! START GAME function
 
 const startGame = () => {
-    console.log("partito!");
-    
+
+
     button.classList.add("disabled")
     displayNums.innerHTML = "";
 
-    message.innerText = "Ho generato questi numeri, hai 30 secondi per memorizzarli!"
+    message.innerText = `Ho generato questi numeri, hai ${timerCount} secondi per memorizzarli!`
 
-    // creo le celle ed inserisco il numero causale 
+    // creo le celle ed inserisco il numero casuale che ho generato
     for (let i = 0; i < totCells; i++) {
-        
+
         randomNumber = getUniqueRandomNumber(minNum, maxNum, generatedNums, totCells);
 
         generatedNums.push(randomNumber);
-        
+
         cell = generateCell(generatedNums[i]);
         displayNums.appendChild(cell);
 
         myInput = generateMyCell();
         myNums.innerHTML += myInput;
     }
-    
-    const checkInputs = document.querySelectorAll(".my-inputs");
-    console.log(checkInputs)
-    
+
+    // richiamo la funzione timer ogni secondo e stampo il tempo rimanente in pagina
     leftTime = setInterval(() => {
         timerCount = timerFunction(timerCount);
         timer.textContent = timerCount;
     }, 1000)
-    
+
+    //vado a prendere l'elemento del dom che controlla i numeri inseriti dal giocatore
+    const checkInputs = document.querySelectorAll(".my-inputs");
+
+    // valido le risposte del giocatore e restituisco un riscontro
     const confirm = () => {
-        
-        console.log("entrato!")
-        
+
         validationMessage.innerText = "";
-        //validationMessage.classList.remove("text-success");
-        //validationMessage.classList.remove("text-danger");
-        
+        validationMessage.classList.remove("text-success");
+        validationMessage.classList.remove("text-danger");
+
         const userGuesses = [];
         const correctAnswers = [];
-        
-        for (let i=0; i < totCells ; i++) {
-            
+
+        for (let i = 0; i < totCells; i++) {
+
             const field = checkInputs[i];
-            const value = parseInt(field.value)
-            console.log(value)
-            
+            const value = parseInt(field.value);
+
             if (!isNaN(value) && value >= minNum && value <= maxNum && !userGuesses.includes(value)) {
                 userGuesses.push(value);
             }
-            
-        }
-        
-        if (userGuesses.length !== totCells ) {
-            validationMessage.classList.add("text-danger");
-            validationMessage.innerText = "Ci sono valori non validi o duplicati"
+            console.log(userGuesses)
+
         }
 
-        
-        for (let guess of userGuesses) {
-            if(generatedNums.includes(guess)) correctAnswers.push(guess);
+        if (userGuesses.length !== totCells) {
+            validationMessage.classList.remove("text-success")
+            validationMessage.classList.add("text-danger");
+            validationMessage.innerText = "Ci sono dei campi vuoti, non validi o duplicati! Inserisci dei numeri"
+            return
         }
-        
-        if (correctAnswers.length === totCells) {
+
+
+        for (let guess of userGuesses) {
+            if (generatedNums.includes(guess)) correctAnswers.push(guess);
+        }
+
+        if (correctAnswers.length) {
+            validationMessage.classList.remove("text-danger")
             validationMessage.classList.add("text-success")
-        } 
-        validationMessage.innerText = `Hai indovinato ${correctAnswers.length} numeri! (${correctAnswers})`
-        console.log(userGuesses, correctAnswers)
+            if (correctAnswers.length === 1) {
+                validationMessage.innerText = `Hai indovinato ${correctAnswers.length} solo numero... puoi fare meglio!`
+            } else {
+                validationMessage.innerText = `Hai indovinato ${correctAnswers.length} numeri! ${correctAnswers}`
+            }
+        }
+        else {
+            validationMessage.classList.remove("text-success")
+            validationMessage.classList.add("text-danger")
+            validationMessage.innerText = `Non hai ricordato alcun numero... allena ancora la tua memoria!`
+        }
     }
     submitButton.addEventListener("click", confirm);
 }
